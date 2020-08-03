c3256ce74f4a8e4b58ecb1e941bc1bf357222f33
zrui98 <ciguatera.z.rui@gmail.com>
2020-08-03 16:34:14 -0400

    add missing unit tests

--- ui/test/redux/reducers/errorReducer.test.js
+++ ui/test/redux/reducers/errorReducer.test.js
@@ -1,7 +1,7 @@
 import * as types from '../../../src/constants/actions';
-import reducer, { initialState } from '../../../src/redux/reducers/authReducer';
+import reducer, { initialState } from '../../../src/redux/reducers/errorReducer';
 
-describe('redux/reducers/authReducer.js', () => {
+describe('redux/reducers/errorReducer.js', () => {
   describe('when undefined action is called', () => {
     test('then it should return the initial state', () => {
       // Act
@@ -11,29 +11,17 @@ describe('redux/reducers/authReducer.js', () => {
     });
   });
 
-  describe('when LOGIN_REQUEST action is called', () => {
-    test('then isLoading should be set to true', () => {
-      // Arrange
-      const action = { type: types.LOGIN_REQUEST };
-
-      // Act
-      const result = reducer(undefined, action);
-
-      // Assert
-      expect(result).toEqual({ ...initialState, isLoading: true });
-    });
-  });
 
   describe('when LOGIN_FAILED action is called', () => {
-    test('then isLoading should be set to false', () => {
+    test('then error message should be set', () => {
       // Arrange
-      const action = { type: types.LOGIN_FAILED };
+      const action = { type: types.LOGIN_FAILED, errorMessage: 'custom error message' };
 
       // Act
-      const result = reducer({ ...initialState, isLoading: true }, action);
+      const result = reducer({ ...initialState }, action);
 
       // Assert
-      expect(result).toEqual({ ...initialState });
+      expect(result).toEqual({ ...initialState, loginError: 'custom error message' });
     });
   });
 
@@ -43,10 +31,10 @@ describe('redux/reducers/authReducer.js', () => {
       const action = { type: types.LOGIN_SUCCESS };
 
       // Act
-      const result = reducer({ ...initialState, isLoading: true }, action);
+      const result = reducer({ ...initialState, loginError: 'had error before' }, action);
 
       // Assert
-      expect(result).toEqual({ ...initialState });
+      expect(result).toEqual({ ...initialState, loginError: '' });
     });
   });
 });
