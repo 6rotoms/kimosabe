70eb50ebb3e719b031814127aa7f37a5bcb7dc8e
Alex Reymer <alexreymer@gmail.com>
2020-08-05 18:21:30 -0400

    Add constructor to UserController.

--- api/src/main/java/kimosabe/api/controller/UserController.java
+++ api/src/main/java/kimosabe/api/controller/UserController.java
@@ -2,18 +2,22 @@ package kimosabe.api.controller;
 
 import kimosabe.api.model.User;
 import kimosabe.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/changePassword")
     @PreAuthorize("hasRole('ROLE_USER')")
