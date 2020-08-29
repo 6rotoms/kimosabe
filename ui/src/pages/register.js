a55a1505e66f7be264e07ec2628087b5df905a63
Alex Reymer <alexreymer@gmail.com>
2020-08-29 17:30:58 -0400

    Remove DOM access and add errors to state.

--- ui/src/pages/register.js
+++ ui/src/pages/register.js
@@ -3,52 +3,45 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerUser } from '../redux/actions';
 
-
-import '../styles/global.css';
 import '../styles/register.css';
 
 const RegisterPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [cpassword, setCPassword] = useState('');
+    const [inlineErrors, setInlineErrors] = useState({username: '', password: '', compare: ''});
     const registerError = useSelector((state) => state.errors.registerError);
     const isLoading = useSelector((state) => state.auth.isLoading);
     const dispatch = useDispatch();
 
-    const fieldValidation = () => {
-      document.getElementById('error-username').innerText = '';
-      document.getElementById('error-password').innerText = '';
-      document.getElementById('error-compare').innerText = '';
-      let errored = false;
+    const fieldsAreValid = () => {
+      const errors = {};
       if (username.length < 3){
-        document.getElementById('error-username').innerText = 'Username should be at least 3 characters';
-        errored = true;
+        errors['username'] = 'Username should be at least 3 characters';
       }
       if (password.length < 3){
-        document.getElementById('error-password').innerText = 'Password should be at least 3 characters';
-        errored = true;
+        errors['password'] = 'Password should be at least 3 characters';
       }
       if (password.localeCompare(cpassword) !== 0){
-        document.getElementById('error-compare').innerText = 'Passwords should match';
-        errored = true;
+        errors['compare'] = 'Passwords should match';
       }
-      return !errored;
+      setInlineErrors(errors);
+      return Object.keys(errors).length === 0;
     };
 
     return(<div className='main-content'>
       <form onSubmit={(e) => {
         e.preventDefault();
-        if(fieldValidation()){
+        if(fieldsAreValid()){
           dispatch(registerUser({ username, password }));
         }
         }} className='register-form'>
         <input type='text' value={username} onChange={(event) => setUsername(event.target.value)} placeholder='Username' className='input-field register-form-input'/>
-        <span className='error' id='error-username'></span>
-        <span data-testid="error-span" className='error'>{registerError}</span>
+        <span className='error' id='error-username'>{inlineErrors.username || registerError }</span>
         <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} placeholder='Password' className='input-field register-form-input'/>
-        <span className='error' id='error-password'></span>
+        <span className='error' id='error-password'>{inlineErrors.password}</span>
         <input type='password' value={cpassword} onChange={(event) => setCPassword(event.target.value)} placeholder='Confirm Password' className='input-field register-form-input'/>
-        <span className='error' id='error-compare'></span>
+        <span className='error' id='error-compare'>{inlineErrors.compare}</span>
         {isLoading ? <span className='loading-text'>Loading...</span> : null }
         <input type="submit" value="Register" className='input-button register-form-input'/>
       </form>
