20ef76743b0d68db95a7c6ff208ddb7c12d418fb
Alex Reymer <alexreymer@gmail.com>
2020-09-10 15:54:00 -0400

    Create UI for header and login page.

--- ui/src/components/header.js
+++ ui/src/components/header.js
@@ -1,34 +1,18 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import '../styles/header.css';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+    <header className='header-container'>
+      <div className='header-title'>
+        <Link to="/" className='header-title__text'>{siteTitle}</Link>
+        <input type='text' placeholder='Look For Group...' className='search-field header-title__search'/>
+      </div>
+      <div className='header-login'>
+        <Link to="/login/" className='input-button header-login__button'>Login</Link>
+      </div>
+    </header>
 );
 
 Header.propTypes = {
