02f0982bb4b769ede589dbd91aaac8d1f49f558f
zrui98 <ciguatera.z.rui@gmail.com>
2020-11-23 16:01:57 -0500

    Implement groups tile

--- ui/src/pages/userprofile.js
+++ ui/src/pages/userprofile.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import history from '../history';
 import Tile from '../components/Tile';
 import UserBiography from '../components/UserBiography';
+import userService from '../services/userService';
 import '../styles/userprofile.css';
 
-const UserProfilePage = () => {
+const UserProfilePage = ({ username }) => {
+  const [groupItems, setGroupItems] = useState('');
+
+  useEffect(() => {
+    const getGroups = async () => {
+      const response = await userService.getGroups({ username });
+      if (response.status !== 200) {
+        return;
+      }
+      const newGroupItems = response.body.map((group) => (
+        <div key={group.groupId} className="user-group" onClick={() => history.push(`/group/${group.groupId}`)}>
+          <img className="group-coverart" src={group.coverUrl} alt={group.coverUrl} />
+          <div className="user-group_title">
+            {group.groupName.length > 34 ? `${group.groupName.substring(0, 32)}...` : group.groupName}
+          </div>
+        </div>
+      ));
+      setGroupItems(newGroupItems);
+    };
+
+    getGroups();
+  }, [username]);
+
   return (
     <div className="userprofile-container">
       <div className="col1">
@@ -15,7 +39,11 @@ const UserProfilePage = () => {
         <Tile></Tile>
       </div>
       <div className="col3">
-        <Tile title="Groups" titleAlign="left"></Tile>
+        <Tile title="Groups" titleAlign="left">
+          <div className="groups-container" data-testid="users-groups">
+            {groupItems}
+          </div>
+        </Tile>
       </div>
     </div>
   );
