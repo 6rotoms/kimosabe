6fd49bdabf0793e143a5f697ae0fa0f01ee4bcb6
zrui98 <ciguatera.z.rui@gmail.com>
2021-01-11 00:31:22 -0500

    Add master merge pipeline

--- api/src/main/java/kimosabe/api/config/WebConfig.java
+++ api/src/main/java/kimosabe/api/config/WebConfig.java
@@ -10,8 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8000") // TODO: Switch to load from Spring Boot properties file
                 .allowCredentials(true);
 
     }
