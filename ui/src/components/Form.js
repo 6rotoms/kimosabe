8460e270c508ead991c6338ec169a13a14d70336
Alex Reymer <alexreymer@gmail.com>
2021-01-17 14:33:38 -0500

    Finish up with login/register rework

--- ui/src/components/Form.js
+++ ui/src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = (props) => {
   const { className, ...other } = props;
 
-  return <form {...other} className={`grid grid-rows-auto gap-y-8 ${className}`} />;
+  return <form {...other} className={`grid grid-rows-auto gap-y-8 w-full max-w-lg ${className}`} />;
 };
 
 export default Form;
